a1a442c53a52f6fdee433357d34b996c73f7f1d4
artemiigoldberg <73183439+artemiigoldberg@users.noreply.github.com>
2025-04-05 08:52:38 +0300

    add new files

--- app/client/rollup.config.mjs
+++ app/client/rollup.config.mjs
@@ -1,18 +1,12 @@
-import babel from '@rollup/plugin-babel';
-const pages = ['hello.js', 'login.js', 'registration.js'];
-
-const pluginList = [babel({ babelHelpers: 'bundled' })];
-const export_page = pages.reduce((acc, item) => {
-    acc.push({
-        input: `./src/${item}`,
-        output: {
-            file: `../server/www/js/${item}`,
-            format: 'cjs',
-            sourcemap: 'inline',
-        },
-        plugins: pluginList,
-    });
-    return acc;
-}, []);
-
-export default export_page;
+{
+    "plugins": [
+      "babel-plugin-transform-redom-jsx",
+      [
+        "transform-react-jsx",
+        {
+          "pragma": "el"
+        }
+      ]
+    ],
+    "presets": ["@babel/preset-env"]
+}
